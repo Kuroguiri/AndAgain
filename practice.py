4d26e31696220b3a405189e384e707004ddff5d7
KuroGuiri <iotglider@gmail.com>
2024-02-09 01:14:36 +0600

    Первый комет

---
+++ practice.py
@@ -0,0 +1,20 @@
+# SayMyName = input("Say my name:\n")
+# match SayMyName:
+#     case "Vannoberg":
+#         print("U gd dm rght")
+#     case _ if SayMyName.isdigit() or "R2D2":
+#         print("C-3-P-O, r u?")
+#     case _:
+#         print("R u srsly?")
+
+# def Star(name, count, fart):
+#     print("And "+name+" gets")
+#     while count > 0:
+#         print("       ~",fart)
+#         count -=1
+#
+# Star("Billy", 4, "Pggsrsr")
+
+listek = ([1]*2+["Tim"]*2+[3]*2)*2
+print(listek)
+print(listek[3])
